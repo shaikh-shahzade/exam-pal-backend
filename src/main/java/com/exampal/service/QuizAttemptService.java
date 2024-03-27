fd31ce178181dd3c2877d5b994ca95e20b9a62ba
shaikh shahzade <shahzade10@gmail.com>
2024-03-28 00:23:37 +0530

    Quiz Attempt service

---
+++ src/main/java/com/exampal/service/QuizAttemptService.java
@@ -0,0 +1,8 @@
+package com.exampal.service;
+
+import com.exampal.model.quiz.QuizAttempt;
+
+public interface QuizAttemptService {
+
+	public QuizAttempt createAttempt(QuizAttempt quizAttempt);
+}
---
+++ src/main/java/com/exampal/service/impl/QuizAttemptImpl.java
@@ -0,0 +1,17 @@
+package com.exampal.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.exampal.model.quiz.QuizAttempt;
+import com.exampal.service.QuizAttemptService;
+
+@Service
+public class QuizAttemptImpl implements QuizAttemptService{
+
+	@Override
+	public QuizAttempt createAttempt(QuizAttempt quizAttempt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
