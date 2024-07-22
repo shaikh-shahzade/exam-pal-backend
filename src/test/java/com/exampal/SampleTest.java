bba844b7d8895daf115e3a1556a1ca06d51d7ff9
shaikh shahzade <shahzade10@gmail.com>
2024-07-22 23:42:49 +0530

    Adhoc Test case: Sample test class

---
+++ src/test/java/com/exampal/SampleTest.java
@@ -0,0 +1,20 @@
+package com.exampal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SampleTest {
+
+	@Test
+	public void when_get_category_by_ID()
+	{
+			//implement test case here
+	}
+}
